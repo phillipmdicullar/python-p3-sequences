81a4dd7073ecb701909535aa18a9faf2b0440ea1
Philip Emdokolo <phillip.emdokolo@student.moringaschool.com>
2024-11-27 20:08:20 +0300

    fibonacci sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print("[]")
+    elif length == 1:
+        print("[0]")
+    elif length == 2:
+        print("[0, 1]")
+    else:
+        fibonacci_sequence = [0, 1]
+        for i in range(2, length):
+            fibonacci_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(fibonacci_number)
+        print(fibonacci_sequence)
+print_fibonacci(9)
